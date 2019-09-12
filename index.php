d571a7d08716a80cbd66de9d9da0b5d8b7934332
diplopito <26707508+diplopito@users.noreply.github.com>
2019-09-12 22:08:24 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,209 @@
+<?php
+
+use Phalcon\Mvc\Micro;
+use Phalcon\Db\Adapter\Pdo\Mysql as MysqlAdapter;
+
+$loader = new \Phalcon\Loader();
+
+$loader->registerDirs(
+    [
+        __DIR__ . '/models/',
+    ]
+)->register();
+
+$app = new Micro();
+
+
+
+// Setup the database service
+$app['db'] = function () {
+    return new MysqlAdapter(
+        [
+            'host'     => 'localhost',
+            'username' => 'root',
+            'password' => '',
+            'dbname'   => 'demo',
+        ]
+    );
+};
+
+$app->get(
+    "/",
+    function () {
+        echo "<h1>Welcome</h1>";
+    }
+);
+
+$app->get(
+    "/editModel",
+    function() use ($app) {
+        $post = [
+            'id' => 1,
+            'first_name' => 'Johhny',
+        ];
+
+
+    }
+);
+
+$app->get(
+    "/search",
+    function () use ($app) {
+        $users = $app['db']->query("SELECT id, first_name, last_name FROM users");
+
+        while ($user = $users->fetch()) {
+            echo $user['first_name'] . "<br>";
+         }
+    }
+);
+
+$app->get(
+    "/show/{id:[0-9]}",
+    function ($id) use ($app) {
+        $users = new Users();
+        $user = $users::findFirst($id);
+        echo $user->first_name;  // echo $user->firstName will return empty
+    }
+);
+
+$app->get(
+    "/saveModel/{name}",
+    function($name) use ($app) {
+
+        $john = Users::findFirst(1);
+        echo "Current name: " . $john->first_name . 
+        " will be changed to $name<br>";
+
+        $post = [
+            'id' => 1,
+            'first_name' => $name
+        ];
+
+        $user = new Users();
+
+        $user->assign($post);
+
+        if ($user->save() === false) {
+            echo "John doesn't like his new name";
+            $messages = $user->getMessages();
+
+            foreach ($messages as $message) {
+                echo "<br>" . $message;
+            }
+        } else {
+            $john = Users::findFirst(1);
+            echo "New name: " . $john->first_name;            
+        }
+    }
+);
+
+$app->get(
+    "/updateModel/{name}",
+    function($name) use ($app) {
+
+        $john = Users::findFirst(1);
+        echo "Current name: " . $john->first_name . 
+        " will be changed to $name<br>";
+
+        $post = [
+            'id' => 1,
+            'first_name' => $name
+        ];
+
+        $user = new Users();
+
+        $user->assign($post);
+
+        if ($user->update() === false) {
+            echo "John doesn't want to update his name! <br>";
+            $messages = $user->getMessages();
+
+            foreach ($messages as $message) {
+                echo "<br>" . $message;
+            }
+        } else {
+            $john = Users::findFirst(1);
+            echo "New name: " . $john->first_name;            
+        }
+    }
+);
+
+$app->get(
+    "/updateModelFindFirst/{name}",
+    function($name) use ($app) {
+
+        $john = Users::findFirst(1);
+        echo "Current name: " . $john->first_name . 
+        " will be changed to $name<br>";
+
+        $john->assign(
+            [
+                'first_name' => $name
+            ],
+            [
+                'first_name',
+                'last_name'
+            ]
+         );
+
+         if ($john->update() === false) {
+            echo "John doesn't want to update his name! <br>";
+            $messages = $john->getMessages();
+
+            foreach ($messages as $message) {
+                echo "<br>" . $message;
+            }
+        } else {
+            $john = Users::findFirst(1);
+            echo "New name: " . $john->first_name;            
+        }
+    }
+);
+
+$app->get(
+    "/updateModelWhitelist/{name}",
+    function($name) use ($app) {
+
+        $john = Users::findFirst(1);
+        echo "Current name: " . $john->first_name . 
+        " will be changed to $name<br>";
+
+        $post = [
+            'id' => 1,
+            'first_name' => $name
+        ];
+
+        $user = new Users();
+
+        $user->assign($post,
+            [
+                'first_name',
+                'last_name'
+            ]
+         );
+
+         if ($user->update() === false) {
+            echo "John doesn't want to update his name! <br>";
+            $messages = $user->getMessages();
+
+            foreach ($messages as $message) {
+                echo "<br>" . $message;
+            }
+        } else {
+            $john = Users::findFirst(1);
+            echo "New name: " . $john->first_name;            
+        }
+
+    }
+);
+
+$app->get(
+    "/updateRaw/{name}",
+    function($name) use ($app) {
+        $app['db']->query("UPDATE users SET first_name = '$name' WHERE id = 1");
+        $johhnyRaw = Users::findFirst(1);
+        echo $johhnyRaw->first_name;
+    }
+);
+
+$app->handle($_SERVER["REQUEST_URI"]);
